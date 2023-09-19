3d3d230b2187121bab641531341ca957c5a8c50f
realtun <maumai07@gmail.com>
2023-09-19 10:18:51 +0700

    update

--- bai1.php
+++ bai1.php
@@ -4,18 +4,27 @@
         return $carry;
     }
 
-    function subtract($temp, $item){
-        $temp -= $item;
+    function subtract($arr) : int{
+        $temp = $arr[0];
+        for($i = 1; $i < count($arr); $i++){
+            $temp -= $arr[$i];
+        }
         return $temp;
     }
 
-    function multiplication($temp, $item){
-        $temp *= $item;
+    function multiplication($arr){
+        $temp = 1;
+        foreach($arr as $value){
+            $temp *= $value;
+        }
         return $temp;
     }
 
-    function division($temp, $item){
-        $temp /= $item;
+    function division($arr) : float{
+        $temp = $arr[0];
+        for($i = 1; $i < count($arr); $i++){
+            $temp /= $arr[$i];
+        }
         return $temp;
     }
 
@@ -26,7 +35,7 @@
     echo '</pre>';
 
     echo 'Sum of array: ' . array_reduce($arr, 'sum') . '<br>';
-    echo 'Subtract of array: ' . array_reduce($arr, 'subtract') . '<br>';
-    echo 'Multiplication of array: ' . array_reduce($arr, 'multiplication') . '<br>';
-    echo 'Division of array: ' . array_reduce($arr, 'division') . '<br>';
+    echo 'Subtract of array: ' . subtract($arr) . '<br>';
+    echo 'Multiplication of array: ' . multiplication($arr) . '<br>';
+    echo 'Division of array: ' . division($arr) . '<br>';
     
